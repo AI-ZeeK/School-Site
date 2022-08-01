ad00827a09a908edba7c2e4e304beed3a221a8ec
Isaac Williams <isaaciyaye174@gmail.com>
2022-08-01 15:57:05 +0100

    New Commit
    adding new images an html documents to already existing

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,32 @@
 const menuBtn = document.getElementById('menu-btn');
 const closeBtn = document.getElementById('menu-close');
 const navHeader = document.getElementById('mobile-nav');
+const header = document.querySelector('.header');
 
 menuBtn.addEventListener('click', openNav);
 closeBtn.addEventListener('click', closeNav);
 
+
 function openNav(){
     navHeader.classList.add('active');
-    menuBtn.style.setProperty('display', 'none');
+    menuBtn.classList.add('menu-btn-active');
 }
 
 function closeNav(){
     navHeader.classList.remove('active');
-    menuBtn.style.setProperty('display', 'block');
+    menuBtn.classList.remove('menu-btn-active');
 }
+
+document.addEventListener('click', (event) => {
+    if(!event.target.closest('.navbar') && !event.target.closest('.navbar'))
+        navHeader.classList.remove('active');
+        menuBtn.classList.remove('menu-btn-active');
+    // body.style.setProperty('background-color', 'red')
+    console.log('clicked')
+})
+
+// document.addEventListener('click' , (event) => {;
+//     if(!event.target.closest(".header") && !event.target.closest(".navbar")){
+//         navHeader.classList.remove('active');
+//     }
+// })
